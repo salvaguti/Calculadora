e0b0387f46dae09cbfca0aa7311c6da7a79b279d
Salva Gutiérrez <salva.guti@gmail.com>
2018-12-22 07:27:32 +0100

    Primera Versión de la Calculadora
    Creación del proyecto y definición básica de la clase Calculator

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Calculator
+    {
+        public double oper1;
+        public double oper2;
+        private double result;
+
+        public Calculator(double v1, double v2)
+        {
+            oper1 = v1;
+            oper2 = v2;
+        }
+        public double Suma()
+        {
+            result = oper1 + oper2;
+            return result;
+        }
+            
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Ejemplo de uso
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculadora = new Calculator(10,20);
+            Console.WriteLine("Resultado: {0}", calculadora.Suma());
+            Console.ReadKey();
+        }
+    }
+}
